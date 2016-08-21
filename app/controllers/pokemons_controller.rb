463ebf6ad43da16fdb6deb8e06a1b7c545599c6a
JESUA BETANCOR ALEMAN <jbetancor.0011@gmail.com>
2016-08-21 21:10:54 +0100

    Start pagination

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,12 +1,14 @@
 class PokemonsController < ApplicationController
 
+  @@total_pokemon = 721
+
   def index
     @num = (params.has_key?(:id)) ? params[:id].to_i : 1
   end
 
   def apicall
     require 'open-uri'
-    @get    = (params.has_key?(:id)) ? params[:id].to_s : rand(1..721).to_s
+    @get    = (params.has_key?(:id)) ? params[:id].to_s : rand(1..@@total_pokemon).to_s
     @render = (params.has_key?(:id)) ? '/pokemons/pokeitem' : '/pokemons/pokedata'
     @pokemon = JSON.load(open("http://pokeapi.co/api/v2/pokemon/" + @get))
     respond_to do |format|
